8c6aa716582717eb2853a839b06513586dc8ddaf
honghuiseung <huiseung1082@daum.net>
2024-01-12 17:30:45 +0900

    sec06

---
+++ chap06/src/sec05/exam02/CalculatorExample.java
@@ -0,0 +1,15 @@
+package sec05.exam02;
+
+public class CalculatorExample {
+
+	public static void main(String[] args) {
+		double result1 = 10 * 10 * Carculator.pi;
+		int result2 = Carculator.plus(10, 5);
+		int result3 = Carculator.minus(10, 5);
+		
+		System.out.println("result : " + result1);
+		System.out.println("result : " + result2);
+		System.out.println("result : " + result3);
+	}
+
+}
---
+++ chap06/src/sec06/exam04/package2/C.java
@@ -0,0 +1,10 @@
+package sec06.exam04.package2;
+
+import sec06.exam04.package1.A;
+
+public class C {
+	// 필드
+	A a1 = new A(true);
+	// A a2 = new A(1); 같은패키지가 아니라서 defalt 접근제한자에 막힘
+	// A a3 = new A("문자열");
+}
